9b16f62e306ca8c77eb47799c0789fc531ffcfd5
ShuumaIwamoto <iwa06ki12da@gmail.com>
2021-07-14 22:23:30 +0900

    commit

--- melpit/app/View/Components/Header.php
+++ melpit/app/View/Components/Header.php
@@ -2,6 +2,8 @@
 
 namespace App\View\Components;
 
+use App\Models\PrimaryCategory;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\Component;
 
@@ -25,6 +27,21 @@ class Header extends Component
     public function render()
     {
         $user = Auth::user();
-        return view('components.header')->with('user',$user);
+
+        $categories = PrimaryCategory::query()
+            ->with([
+                'secondaryCategories' => function ($query) {
+                    $query->orderBy('sort_no');
+                }
+            ])
+            ->orderBy('sort_no')
+            ->get();
+        //ヘッダーに入力された値を渡す
+        $defaults = [
+            'category' => Request::input('category', ''),
+            'keyword'  => Request::input('keyword', ''),
+        ];
+        return view('components.header')->with('user', $user)->with('categories', $categories)
+            ->with('defaults', $defaults);
     }
 }
